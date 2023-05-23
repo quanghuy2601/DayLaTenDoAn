f393a1d7ef596fda1714a96de3ec4d8c411be819
quanghuy2601 <quanghuy7927@gmail.com>
2023-05-23 07:59:53 +0700

    Add project files.

---
+++ DayLaTenDoAn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DayLaTenDoAn.MvcApplication" Language="C#" %>
---
+++ DayLaTenDoAn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DayLaTenDoAn.Startup))]
+namespace DayLaTenDoAn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
